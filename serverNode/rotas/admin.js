b0af461d231d41c8e70dd2794eb4a98d338ac08f
alinebeatrizw <alinewille31@gmail.com>
2020-12-02 16:14:06 -0300

    ok

--- serverNode/rotas/admin.js
+++ serverNode/rotas/admin.js
@@ -319,67 +319,6 @@ router.get("/google/callback", function (req, res) {
   });
   
 
-
-
-
-// Calling event routes when request first param is event
-router.get('/events', function (req, res) {
-  EventModel.fetchEvents({year: req.query.year, month: req.query.month},function (err, events) {
-      if(err){
-          res.json({status: 4000, errors: errors});
-      }else{
-          res.json({status: 2000, data: events});
-      }
-  });
-});
-//Create Event
-router.post('/events', function (req, res) {
-  var newEvent = new EventModel({
-      year: req.body.year,
-      month: req.body.month,
-      date: req.body.date,
-      weeksIndex: req.body.weeksIndex,
-      dayIndex: req.body.dayIndex,
-      title: req.body.title,
-      description: req.body.description
-  });
-  EventModel.createEvent(newEvent, function (err, event) {
-      if(err){
-          res.json({status: 4000, message: "Failed to create event"});
-      }else{
-          //Broadcasting to all when a new event successfully created
-          io.sockets.emit('create_event', event);
-          res.json({status: 2001, message: "Event created successfully", data: event});
-      }
-  });
-});
-
-//Edit Event
-router.put('/events/:id', function (req, res) {
-  EventModel.editEvent({id: req.params.id, title: req.body.title, description: req.body.description}, function (err, event) {
-      if(err){
-          res.json({status: 4000, message: "Failed to update event"});
-      }else{
-          //Broadcasting to all when an event get updated
-          io.sockets.emit('edit_event', req.body);
-          res.json({status: 2002, message: "Event updated successfully", data: req.body});
-      }
-  });
-});
-
-//Delete Event
-router.delete('/events/:id', function (req, res) {
-  EventModel.deleteEvent(req.params.id, function (err, event) {
-      if(err){
-          res.json({status: 4000, message: "Failed to delete event"});
-      }else{
-          //Broadcasting to all when an event deleted
-          io.sockets.emit('delete_event', req.query);
-          res.json({status: 2003, message: "Event deleted successfully"});
-      }
-  });
-});
-
 router.get("/agenda", (req,res)=>{
   fs.readFile('./fullcalendar.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
