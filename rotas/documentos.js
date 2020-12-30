3d890b4481b4c5c5ec0c1bfb05f457169a21b7f0
alinebeatrizw <alinewille31@gmail.com>
2020-12-30 13:42:22 -0300

    refatorando

--- rotas/documentos.js
+++ rotas/documentos.js
@@ -1,27 +1,10 @@
 
 const express = require("express")//express
 const router = express.Router()//colocando a função de rotas do express dentro da constante
-const mongoose = require("mongoose")//mongoose
-require("../models/Cliente")//model de cliente
-const Cliente = mongoose.model("clientes")//colocando o model dentro da costante
-require("../models/Processo")
-const Processo = mongoose.model("processos")
-const multer = require("multer");
-const OAuth2Data = require("../credentials.json");
-var name,pic
-const { google } = require("googleapis");
-var async = require("async");
-const fs = require("fs");
-
-
-
-//se eu nao conseguir colocar dentro da rota
-//tem que alterar o caminho na api pra voltar a funcionar no localhost
 
 router.get("/", (req,res)=>{
     res.render("documentos/google-drive")
 })
-
 
 //exportando as rotas
 module.exports = router
